16a9fc221061a75981b25271a2ab278551453f3e
Radoniaina24 <andriambolaradoniainamichael@gmail.com>
2025-04-09 09:06:34 +0300

    v6.1

--- controllers/appicationController.js
+++ controllers/appicationController.js
@@ -3,11 +3,12 @@ const JobPost = require("../models/jobPostModel");
 const Recruiter = require("../models/recruiterModel");
 const Candidate = require("../models/candidateModel");
 const cloudinary = require("cloudinary").v2;
+
 const applyToJob = async (req, res) => {
   if (!req.files) {
-    return res
-      .status(400)
-      .json({ message: "Veuillez télécharger les fichiers requis." });
+    return res.status(400).json({
+      message: "Please upload the required files.",
+    });
   }
 
   const uploadedFiles = {};
@@ -18,39 +19,37 @@ const applyToJob = async (req, res) => {
       type: req.files[key][0].mimetype.startsWith("image/") ? "image" : "pdf",
     };
   });
+
   try {
     const { jobId } = req.params;
     const { coverLetter } = req.body;
     const userId = req.user.id;
-    // console.log(userId);
-    // Vérifier si le candidat existe
+
     const candidate = await Candidate.findOne({ user: userId });
     if (!candidate) {
-      return res.status(404).json({ message: "Candidat introuvable." });
-    }
-    // Vérifier si l'annonce existe
-    const jobPost = await JobPost.findById(jobId).populate("recruiter");
-    if (!jobPost) {
-      return res.status(404).json({ message: "Annonce introuvable." });
+      return res.status(404).json({ message: "Candidate not found." });
     }
 
-    // Vérifier si le candidat a déjà postulé
+    const jobPost = await JobPost.findById(jobId).populate("recruiter");
+    if (!jobPost) {
+      return res.status(404).json({ message: "Job post not found." });
+    }
+
     const existingApplication = await Application.findOne({
       candidate: candidate._id,
       jobPost: jobId,
     });
 
     if (existingApplication) {
-      // Suppression du fichier dans cloudinary
       const resourceType = uploadedFiles.file.type === "pdf" ? "raw" : "image";
       await cloudinary.uploader.destroy(uploadedFiles.file.publicId, {
         resource_type: resourceType,
       });
       return res
         .status(400)
-        .json({ message: "Vous avez déjà postulé à cette annonce." });
+        .json({ message: "You have already applied to this job." });
     }
-    // Créer une nouvelle candidature
+
     const application = new Application({
       candidate: candidate._id,
       jobPost: jobId,
@@ -60,44 +59,51 @@ const applyToJob = async (req, res) => {
     });
 
     await application.save();
-    res
-      .status(201)
-      .json({ message: "Candidature envoyée avec succès.", application });
+
+    res.status(201).json({
+      message: "Application submitted successfully.",
+      application,
+    });
   } catch (error) {
-    res.status(500).json({ message: "Erreur serveur", error: error.message });
+    res.status(500).json({
+      message: "Server error.",
+      error: error.message,
+    });
   }
 };
+
 const getCandidateApplications = async (req, res) => {
   try {
     const userId = req.user.id;
-    // Vérifier que l'utilisateur est un candidate
-    const candidateId = await Candidate.findOne({ user: userId });
-    if (!candidateId) {
+    const candidate = await Candidate.findOne({ user: userId });
+
+    if (!candidate) {
       return res
         .status(403)
-        .json({ message: "Accès refusé. Candidate requis." });
+        .json({ message: "Access denied. Candidate access required." });
     }
-    // Récupérer toutes les candidature du candidat
+
     let { page = 1, limit = 10, search } = req.query;
-    // Validation et conversion des paramètres en nombres
     page = Math.max(1, parseInt(page, 10) || 1);
-    limit = Math.max(1, Math.min(parseInt(limit, 10) || 10, 100)); // Limite à 100 max
+    limit = Math.max(1, Math.min(parseInt(limit, 10) || 10, 100));
+
     const searchQuery = search
       ? { $or: [{ coverLetter: { $regex: search, $options: "i" } }] }
       : {};
-    // Exécuter les requêtes en parallèle pour améliorer les performances
+
     const [totalJobApplication, jobApplication] = await Promise.all([
-      Application.countDocuments({ candidate: candidateId, ...searchQuery }),
-      Application.find({ candidate: candidateId, ...searchQuery })
+      Application.countDocuments({ candidate: candidate._id, ...searchQuery }),
+      Application.find({ candidate: candidate._id, ...searchQuery })
         .populate({
           path: "jobPost",
-          populate: { path: "recruiter" },
-          populate: { path: "sector" },
+          populate: [{ path: "recruiter" }, { path: "sector" }],
         })
         .skip((page - 1) * limit)
         .limit(limit),
     ]);
+
     const totalPages = Math.ceil(totalJobApplication / limit);
+
     res.status(200).json({
       success: true,
       totalJobApplication,
@@ -106,50 +112,65 @@ const getCandidateApplications = async (req, res) => {
       jobApplication,
     });
   } catch (error) {
-    res.status(500).json({ message: "Erreur serveur", error: error.message });
+    res.status(500).json({
+      message: "Server error.",
+      error: error.message,
+    });
   }
 };
+
 const deleteApplication = async (req, res) => {
   try {
-    const candidateId = req.user.id;
+    const userId = req.user.id;
     const { id } = req.params;
+    const candidate = await Candidate.findOne({ user: userId });
 
-    // Vérifier si la candidature existe et appartient au candidat
+    if (!candidate) {
+      return res
+        .status(403)
+        .json({ message: "Access denied. Candidate access required." });
+    }
+
     const application = await Application.findOne({
       _id: id,
-      candidate: candidateId,
+      candidate: candidate._id,
     });
 
     if (!application) {
       return res
         .status(404)
-        .json({ message: "Candidature introuvable ou non autorisée." });
+        .json({ message: "Application not found or unauthorized." });
     }
 
     await Application.deleteOne({ _id: id });
-    res.status(200).json({ message: "Candidature supprimée avec succès." });
+    res.status(200).json({ message: "Application deleted successfully." });
   } catch (error) {
-    res.status(500).json({ message: "Erreur serveur", error: error.message });
+    res.status(500).json({
+      message: "Server error.",
+      error: error.message,
+    });
   }
 };
+
 const getRecruiterApplications = async (req, res) => {
   try {
     const userId = req.user.id;
-    // Vérifier que l'utilisateur est un recruteur
     const recruiter = await Recruiter.findOne({ user: userId });
+
     if (!recruiter) {
       return res
         .status(403)
-        .json({ message: "Accès refusé. Recruteur requis." });
+        .json({ message: "Access denied. Recruiter access required." });
     }
+
     let { page = 1, limit = 10, search } = req.query;
-    // Validation et conversion des paramètres en nombres
     page = Math.max(1, parseInt(page, 10) || 1);
-    limit = Math.max(1, Math.min(parseInt(limit, 10) || 10, 100)); // Limite à 100 max
+    limit = Math.max(1, Math.min(parseInt(limit, 10) || 10, 100));
+
     const searchQuery = search
       ? { $or: [{ status: { $regex: search, $options: "i" } }] }
       : {};
-    // Exécuter les requêtes en parallèle pour améliorer les performances
+
     const [totalJobApplication, jobApplication] = await Promise.all([
       Application.countDocuments({ recruiter: recruiter._id, ...searchQuery }),
       Application.find({ recruiter: recruiter._id, ...searchQuery })
@@ -159,13 +180,14 @@ const getRecruiterApplications = async (req, res) => {
         })
         .populate({
           path: "jobPost",
-          populate: { path: "category" },
-          populate: { path: "sector", select: "name" },
+          populate: [{ path: "sector", select: "name" }],
         })
         .skip((page - 1) * limit)
         .limit(limit),
     ]);
+
     const totalPages = Math.ceil(totalJobApplication / limit);
+
     res.status(200).json({
       success: true,
       totalJobApplication,
@@ -173,32 +195,31 @@ const getRecruiterApplications = async (req, res) => {
       currentPage: page,
       jobApplication,
     });
-
-    res.status(200).json(applications);
   } catch (error) {
-    res.status(500).json({ message: "Erreur serveur", error: error.message });
+    res.status(500).json({
+      message: "Server error.",
+      error: error.message,
+    });
   }
 };
+
 const updateApplicationsById = async (req, res, next) => {
   try {
     const { status } = req.body;
     const userId = req.user.id;
 
-    // Check if the user is a recruiter
     const recruiter = await Recruiter.findOne({ user: userId });
     if (!recruiter) {
       return res
         .status(403)
-        .json({ message: "Access denied. Recruiter required." });
+        .json({ message: "Access denied. Recruiter access required." });
     }
 
-    // Find the application related to the recruiter
     const application = await Application.findOne({ recruiter: recruiter._id });
     if (!application) {
       return res.status(404).json({ message: "Application not found." });
     }
 
-    // Update the application status
     application.status = status || application.status;
     await application.save();
 
@@ -208,6 +229,14 @@ const updateApplicationsById = async (req, res, next) => {
   } catch (error) {
     next(error);
   }
+};
+
+module.exports = {
+  applyToJob,
+  getCandidateApplications,
+  deleteApplication,
+  getRecruiterApplications,
+  updateApplicationsById,
 };
 
 module.exports = {
